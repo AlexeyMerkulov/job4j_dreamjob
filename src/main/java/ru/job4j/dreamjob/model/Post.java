f52df75c0721e099f05f16d4c5d8f10ae52a57be
AlexeyMerkulov <alexeymerkulovv@gmail.com>
2022-07-22 10:28:04 +0300

    1. Формы. Поля ввода. [#504853]

--- src/main/java/ru/job4j/dreamjob/model/Post.java
+++ src/main/java/ru/job4j/dreamjob/model/Post.java
@@ -8,6 +8,7 @@ public class Post {
     private String name;
     private String description;
     private LocalDateTime created;
+    private boolean visible;
 
     public Post() { }
 
@@ -48,6 +49,14 @@ public class Post {
 
     public void setCreated(LocalDateTime created) {
         this.created = created;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
     }
 
     @Override
