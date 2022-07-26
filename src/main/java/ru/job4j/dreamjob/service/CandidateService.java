675dee34c97909c68bed6ea0343e13cd913b356c
AlexeyMerkulov <alexeymerkulovv@gmail.com>
2022-07-26 22:32:18 +0300

    2. CandidateDbStore. Хранение кандидатов. [#504861]

--- src/main/java/ru/job4j/dreamjob/service/CandidateService.java
+++ src/main/java/ru/job4j/dreamjob/service/CandidateService.java
@@ -3,7 +3,7 @@ package ru.job4j.dreamjob.service;
 import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Service;
 import ru.job4j.dreamjob.model.Candidate;
-import ru.job4j.dreamjob.store.CandidateStore;
+import ru.job4j.dreamjob.store.CandidateDBStore;
 
 import java.util.Collection;
 
@@ -11,9 +11,9 @@ import java.util.Collection;
 @Service
 public class CandidateService {
 
-    private final CandidateStore candidateStore;
+    private final CandidateDBStore candidateStore;
 
-    public CandidateService(CandidateStore candidateStore) {
+    public CandidateService(CandidateDBStore candidateStore) {
         this.candidateStore = candidateStore;
     }
 
---
+++ src/main/java/ru/job4j/dreamjob/store/CandidateDBStore.java
@@ -0,0 +1,98 @@
+package ru.job4j.dreamjob.store;
+
+import net.jcip.annotations.ThreadSafe;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.stereotype.Repository;
+import ru.job4j.dreamjob.model.Candidate;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@ThreadSafe
+@Repository
+public class CandidateDBStore {
+
+    private final BasicDataSource pool;
+
+    public CandidateDBStore(BasicDataSource pool) {
+        this.pool = pool;
+    }
+
+    public List<Candidate> findAll() {
+        List<Candidate> candidates = new ArrayList<>();
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps =  cn.prepareStatement("SELECT * FROM candidate")) {
+            try (ResultSet it = ps.executeQuery()) {
+                while (it.next()) {
+                    candidates.add(getCandidate(it));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return candidates;
+    }
+
+    public Candidate add(Candidate candidate) {
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps =  cn.prepareStatement(
+                     "INSERT INTO candidate(name, description, created, photo) VALUES (?, ?, ?, ?)",
+                     PreparedStatement.RETURN_GENERATED_KEYS)) {
+            setPreparedStatement(ps, candidate);
+            ps.execute();
+            try (ResultSet id = ps.getGeneratedKeys()) {
+                if (id.next()) {
+                    candidate.setId(id.getInt(1));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return candidate;
+    }
+
+    public void update(Candidate candidate) {
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps =  cn.prepareStatement(
+                     "UPDATE candidate SET name = ?, description = ?, created = ?, photo = ? WHERE id = ?")) {
+            setPreparedStatement(ps, candidate);
+            ps.setInt(5, candidate.getId());
+            ps.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Candidate findById(int id) {
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps =  cn.prepareStatement("SELECT * FROM candidate WHERE id = ?")) {
+            ps.setInt(1, id);
+            try (ResultSet it = ps.executeQuery()) {
+                if (it.next()) {
+                    return getCandidate(it);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private Candidate getCandidate(ResultSet resultSet) throws SQLException {
+        return new Candidate(
+                resultSet.getInt("id"),
+                resultSet.getString("name"),
+                resultSet.getString("description"),
+                resultSet.getTimestamp("created").toLocalDateTime(),
+                resultSet.getBytes("photo")
+        );
+    }
+
+    private void setPreparedStatement(PreparedStatement preparedStatement, Candidate candidate) throws SQLException {
+        preparedStatement.setString(1, candidate.getName());
+        preparedStatement.setString(2, candidate.getDescription());
+        preparedStatement.setTimestamp(3, Timestamp.valueOf(candidate.getCreated()));
+        preparedStatement.setBytes(4, candidate.getPhoto());
+    }
+}
